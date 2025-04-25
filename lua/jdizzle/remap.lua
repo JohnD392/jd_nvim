2cf0d084f0ca231e92489cd002b9cb11598405dc
John Dunn <JohnD392@yahoo.com>
2025-04-25 11:16:37 -0700

    Adds diagnostic binding to leader d

--- lua/jdizzle/remap.lua
+++ lua/jdizzle/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Show diagnostics in a floating window
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float(nil, { focusable = false, border = "rounded" })
+end, { desc = "Show diagnostics" })
